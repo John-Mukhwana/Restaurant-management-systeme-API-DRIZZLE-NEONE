151776eaed67698a1df8e550b2c85bc15d9bcfd3
AJD254 <jontexjesus@gmail.com>
2024-06-10 15:20:31 +0300

    restaurant management systen neone serverless

---
+++ src/drizzle/db.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from "./schema";
+
+
+const client = neon(process.env.Database_URL!)
+
+
+
+const db = drizzle(client, {schema, logger: true})// create drizzle instance
+
+export default db;// export the drizzle instance
